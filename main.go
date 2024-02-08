4704e43e2f9d05069941eedb27c77f8b415cecfa
GRPropik <78607300+Gustavitory@users.noreply.github.com>
2024-02-08 10:21:56 -0400

    Recibiendo parametros

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request){//action
+func index(w http.ResponseWriter, r *http.Request){//action con template
 	// fmt.Fprintf(w,"Hola mundo")//por que Fprintf? => porque escribe en un formato especifico, el primer parametro es el writer
 	template,err:=template.ParseFiles("templates/index.html")
 	if err!=nil{
@@ -16,8 +17,17 @@ func index(w http.ResponseWriter, r *http.Request){//action
 	}
 }
 
+func readParameters(w http.ResponseWriter, r *http.Request){
+	namesList, exist := r.URL.Query()["name"]
+	fmt.Printf("%v\n", namesList)
+	if exist{
+		io.WriteString(w,"Hola, "+namesList[0])
+	}else {io.WriteString(w,"Hola, desconocido")}
+}
+
 func main(){
-	http.HandleFunc("/",index); // endpoint
+	http.HandleFunc("/",index) // endpoint
+	http.HandleFunc("/param",readParameters)
 	fmt.Println("El servidor esta escuchando en el puerto 3001")
 	http.ListenAndServe(":3001",nil) // inicia el server en el puerto 3001
 }
